675e133b35d2fb7369d6aeb4e1244f001e13294e
Bailey Capuano <bcapuano@asu.edu>
2022-12-20 22:40:41 -0500

    Speed up hopefully

--- mayhem/fuzz_python.py
+++ mayhem/fuzz_python.py
@@ -4,13 +4,10 @@ import atheris
 import sys
 import fuzz_helpers
 
-with atheris.instrument_imports(include=["python_obfuscator"],
-                                exclude=["python_obfuscator.helpers.variable_name_generator",
-                                "python_obfuscator.helpers.random_datatype"]):
+with atheris.instrument_imports(include=["python_obfuscator"]):
     import python_obfuscator
 
 obfuscator = python_obfuscator.obfuscator()
-
 def TestOneInput(data):
     fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
     obfuscator.obfuscate(fdp.ConsumeRemainingString())
